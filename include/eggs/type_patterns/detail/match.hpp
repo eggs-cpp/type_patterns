17f2c469bf13a9166b90faf5be0d26e1cb0c463c
K-ballo <k@fusionfenix.com>
2013-05-11 02:59:29 -0300

    Tweaking match order
    Signed-off-by: K-ballo <k@fusionfenix.com>

--- include/eggs/type_patterns/detail/match.hpp
+++ include/eggs/type_patterns/detail/match.hpp
@@ -18,7 +18,6 @@
 #include <eggs/type_patterns/placeholders.hpp>
 
 #include <boost/mpl/at.hpp>
-#include <boost/mpl/bool.hpp>
 #include <boost/mpl/has_key.hpp>
 #include <boost/mpl/map.hpp>
 #include <boost/mpl/if.hpp>
@@ -31,132 +30,31 @@
 
 namespace eggs { namespace type_patterns { namespace detail {
 
-    template< typename ...T > struct linear;
-
-    template< typename Pattern, typename Type, typename State >
+    template<
+        typename Pattern, typename Type, typename State
+      , typename Enable = void
+    >
     struct match;
 
-    // templates
-    template< typename P, typename T, typename S, typename E = void >
-    struct match_template
-      : match_false< S >
-    {};
-    template< template< typename... > class P, typename ...PA, typename T, typename S >
-    struct match_template<
-        P< PA... >, T, S
-      , typename boost::enable_if<
-            is_metafunction< P >
-        >::type
-    > : call< P< PA... >, T, S >
-    {};
-    template< template< typename... > class U, typename ...P, typename ...T, typename S >
-    struct match_template<
-        U< P... >, U< T... >, S
-      , typename boost::disable_if<
-            is_metafunction< U >
-        >::type
-    > : match< linear< P... >, linear< T... >, S >
-    {};
-
-    // functions
-    template< typename P, typename T, typename S, typename E = void >
-    struct match_function
-      : match_template< P, T, S >
-    {};
-    template< typename PR, typename ...PA, typename TR, typename ...TA, typename S >
-    struct match_function<
-        PR( PA... ), TR( TA... ), S
-    > : match< linear< PR, PA... >, linear< TR, TA... >, S >
-    {};
-    template< typename PR, typename ...PA, typename TR, typename ...TA, typename S >
-    struct match_function<
-        PR( PA..., ... ), TR( TA..., ... ), S
-    > : match< linear< PR, PA... >, linear< TR, TA... >, S >
-    {};
-
-    // arrays
-    template< typename P, typename T, typename S >
-    struct match_array
-      : match_function< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_array< P[], T[], S >
-      : match< P, T, S >
-    {};
-    template< typename P, typename T, int Size, typename S >
-    struct match_array< P[ Size ], T[ Size ], S >
-      : match< P, T, S >
-    {};
-    template< typename P, typename T, int Size, typename S >
-    struct match_array< P[ N ], T[ Size ], S >
-      : match< P, T, S >
-    {};
-
-    // references
-    template< typename P, typename T, typename S >
-    struct match_reference
-      : match_array< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_reference<
-        P&, T&, S
-    > : match< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_reference<
-        P&&, T&&, S
-    > : match< P, T, S >
-    {};
-
-    // pointers
-    template< typename P, typename T, typename S >
-    struct match_pointer
-      : match_reference< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_pointer<
-        P*, T*, S
-    > : match< P, T, S >
-    {};
-    template< typename P, typename PO, typename T, typename TO, typename S >
-    struct match_pointer<
-        P PO::*, T TO::*, S
-    > : match< linear< P, PO >, linear< T, TO >, S >
-    {};
-
-    // cv-qualifiers
-    template< typename P, typename T, typename S >
-    struct match_cv
-      : match_pointer< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_cv<
-        P const, T const, S
-    > : match< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_cv<
-        P volatile, T volatile, S
-    > : match< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_cv<
-        P const volatile, T const volatile, S
-    > : match< P, T, S >
+    template< typename ...T > struct linear;
+    
+    // terminal
+    template< typename P, typename T, typename S, typename E >
+    struct match
+      : match_bool<
+            boost::is_same< P, T >
+          , S
+        >
     {};
 
     // placeholders
-    template< typename P, typename T, typename S, typename E = void >
-    struct match_placeholder
-      : match_cv< P, T, S >
-    {};
     template< typename T, typename S >
-    struct match_placeholder<
+    struct match<
         _, T, S
     > : match_true< S >
     {};
     template< int P, typename T, typename S >
-    struct match_placeholder<
+    struct match<
         placeholder< P >, T, S
       , typename boost::enable_if_c<
             P != 0 && !boost::mpl::has_key< S, placeholder< P > >::type::value
@@ -169,7 +67,7 @@ namespace eggs { namespace type_patterns { namespace detail {
         >
     {};
     template< int P, typename T, typename S >
-    struct match_placeholder<
+    struct match<
         placeholder< P >, T, S
       , typename boost::enable_if_c<
             P != 0 && boost::mpl::has_key< S, placeholder< P > >::type::value
@@ -183,42 +81,127 @@ namespace eggs { namespace type_patterns { namespace detail {
         >
     {};
 
-    // terminal
+    // cv-qualifiers
     template< typename P, typename T, typename S >
-    struct match_terminal
-      : match_placeholder< P, T, S >
+    struct match<
+        P const, T const, S
+    > : match< P, T, S >
     {};
-    template< typename T, typename S >
-    struct match_terminal<
-        T, T, S
-    > : match_true< S >
+    template< typename P, typename T, typename S >
+    struct match<
+        P volatile, T volatile, S
+    > : match< P, T, S >
+    {};
+    template< typename P, typename T, typename S >
+    struct match<
+        P const volatile, T const volatile, S
+    > : match< P, T, S >
     {};
 
-    // ...finally
+    // pointers
     template< typename P, typename T, typename S >
-    struct match
-      : match_terminal< P, T, S >
+    struct match<
+        P*, T*, S
+    > : match< P, T, S >
+    {};
+    template< typename P, typename PO, typename T, typename TO, typename S >
+    struct match<
+        P PO::*, T TO::*, S
+    > : match< linear< P, PO >, linear< T, TO >, S >
+    {};
+
+    // references
+    template< typename P, typename T, typename S >
+    struct match<
+        P&, T&, S
+    > : match< P, T, S >
+    {};
+    template< typename P, typename T, typename S >
+    struct match<
+        P&&, T&&, S
+    > : match< P, T, S >
+    {};
+
+    // arrays
+    template< typename P, typename T, typename S >
+    struct match< P[], T[], S >
+      : match< P, T, S >
+    {};
+    template< typename P, typename T, int Size, typename S >
+    struct match< P[ Size ], T[ Size ], S >
+      : match< P, T, S >
+    {};
+    template< typename P, typename T, int Size, typename S >
+    struct match< P[ N ], T[ Size ], S >
+      : match< P, T, S >
+    {};
+
+    // functions
+    template< typename PR, typename ...PA, typename TR, typename ...TA, typename S >
+    struct match<
+        PR( PA... ), TR( TA... ), S
+    > : match< linear< PR, PA... >, linear< TR, TA... >, S >
+    {};
+    template< typename PR, typename ...PA, typename TR, typename ...TA, typename S >
+    struct match<
+        PR( PA..., ... ), TR( TA..., ... ), S
+    > : match< linear< PR, PA... >, linear< TR, TA... >, S >
+    {};
+
+    // templates
+    template< template< typename... > class U, typename ...P, typename ...T, typename S >
+    struct match<
+        U< P... >, U< T... >, S
+      , typename boost::disable_if<
+            is_metafunction< U >
+        >::type
+    > : match< linear< P... >, linear< T... >, S >
+    {};
+    template< template< typename... > class P, typename ...PA, typename T, typename S >
+    struct match<
+        P< PA... >, T, S
+      , typename boost::enable_if<
+            is_metafunction< P >
+        >::type
+    > : call< P< PA... >, T, S >
     {};
 
     // linear match
     template< typename P, typename T, typename S >
-    struct match<
+    struct linear_match;
+
+    template< typename P, typename T, typename S >
+    struct linear_match<
         linear< P >, linear< T >, S
     > : match< P, T, S >
     {};
-    template< typename ...T, typename S >
-    struct match<
-        linear< varargs >, linear< T... >, S
+    template< typename T, typename S >
+    struct linear_match<
+        linear< varargs >, linear< T >, S
     > : match_true< S >
     {};
     template< typename P, typename ...PT, typename T, typename ...TT, typename S >
-    struct match<
+    struct linear_match<
         linear< P, PT... >, linear< T, TT... >, S
     > : boost::mpl::if_<
             match< P, T, S >
-          , match< linear< PT... >, linear< TT... >, typename match< P, T, S >::state >
+          , linear_match<
+                linear< PT... >, linear< TT... >
+              , typename match< P, T, S >::state
+            >
           , match_false< S >
         >::type
+    {};
+    template< typename ...T, typename S >
+    struct linear_match<
+        linear< varargs >, linear< T... >, S
+    > : match_true< S >
+    {};
+    //~ we need this specialization or MSVC gets confused
+    template< typename ...P, typename ...T, typename S >
+    struct match<
+        linear< P... >, linear< T... >, S
+    > : linear_match< linear< P... >, linear< T... >, S >
     {};
 
 } } } // namespace eggs::type_patterns::detail
