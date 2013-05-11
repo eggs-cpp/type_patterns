4c4fda9f251c31f5e570d5d4f8da490edbe6b64d
K-ballo <k@fusionfenix.com>
2013-05-11 02:53:10 -0300

    Reversed match order
    Signed-off-by: K-ballo <k@fusionfenix.com>

--- include/eggs/type_patterns/detail/match.hpp
+++ include/eggs/type_patterns/detail/match.hpp
@@ -36,21 +36,119 @@ namespace eggs { namespace type_patterns { namespace detail {
     template< typename Pattern, typename Type, typename State >
     struct match;
 
-    // terminal
-    template< typename P, typename T, typename S >
-    struct match_terminal
+    // templates
+    template< typename P, typename T, typename S, typename E = void >
+    struct match_template
       : match_false< S >
     {};
-    template< typename T, typename S >
-    struct match_terminal<
-        T, T, S
-    > : match_true< S >
+    template< template< typename... > class P, typename ...PA, typename T, typename S >
+    struct match_template<
+        P< PA... >, T, S
+      , typename boost::enable_if<
+            is_metafunction< P >
+        >::type
+    > : call< P< PA... >, T, S >
+    {};
+    template< template< typename... > class U, typename ...P, typename ...T, typename S >
+    struct match_template<
+        U< P... >, U< T... >, S
+      , typename boost::disable_if<
+            is_metafunction< U >
+        >::type
+    > : match< linear< P... >, linear< T... >, S >
+    {};
+
+    // functions
+    template< typename P, typename T, typename S, typename E = void >
+    struct match_function
+      : match_template< P, T, S >
+    {};
+    template< typename PR, typename ...PA, typename TR, typename ...TA, typename S >
+    struct match_function<
+        PR( PA... ), TR( TA... ), S
+    > : match< linear< PR, PA... >, linear< TR, TA... >, S >
+    {};
+    template< typename PR, typename ...PA, typename TR, typename ...TA, typename S >
+    struct match_function<
+        PR( PA..., ... ), TR( TA..., ... ), S
+    > : match< linear< PR, PA... >, linear< TR, TA... >, S >
+    {};
+
+    // arrays
+    template< typename P, typename T, typename S >
+    struct match_array
+      : match_function< P, T, S >
+    {};
+    template< typename P, typename T, typename S >
+    struct match_array< P[], T[], S >
+      : match< P, T, S >
+    {};
+    template< typename P, typename T, int Size, typename S >
+    struct match_array< P[ Size ], T[ Size ], S >
+      : match< P, T, S >
+    {};
+    template< typename P, typename T, int Size, typename S >
+    struct match_array< P[ N ], T[ Size ], S >
+      : match< P, T, S >
+    {};
+
+    // references
+    template< typename P, typename T, typename S >
+    struct match_reference
+      : match_array< P, T, S >
+    {};
+    template< typename P, typename T, typename S >
+    struct match_reference<
+        P&, T&, S
+    > : match< P, T, S >
+    {};
+    template< typename P, typename T, typename S >
+    struct match_reference<
+        P&&, T&&, S
+    > : match< P, T, S >
+    {};
+
+    // pointers
+    template< typename P, typename T, typename S >
+    struct match_pointer
+      : match_reference< P, T, S >
+    {};
+    template< typename P, typename T, typename S >
+    struct match_pointer<
+        P*, T*, S
+    > : match< P, T, S >
+    {};
+    template< typename P, typename PO, typename T, typename TO, typename S >
+    struct match_pointer<
+        P PO::*, T TO::*, S
+    > : match< linear< P, PO >, linear< T, TO >, S >
+    {};
+
+    // cv-qualifiers
+    template< typename P, typename T, typename S >
+    struct match_cv
+      : match_pointer< P, T, S >
+    {};
+    template< typename P, typename T, typename S >
+    struct match_cv<
+        P const, T const, S
+    > : match< P, T, S >
+    {};
+    template< typename P, typename T, typename S >
+    struct match_cv<
+        P volatile, T volatile, S
+    > : match< P, T, S >
+    {};
+    template< typename P, typename T, typename S >
+    struct match_cv<
+        P const volatile, T const volatile, S
+    > : match< P, T, S >
     {};
 
     // placeholders
     template< typename P, typename T, typename S, typename E = void >
     struct match_placeholder
-      : match_terminal< P, T, S >
+      : match_cv< P, T, S >
     {};
     template< typename T, typename S >
     struct match_placeholder<
@@ -85,119 +183,21 @@ namespace eggs { namespace type_patterns { namespace detail {
         >
     {};
 
-    // cv-qualifiers
+    // terminal
     template< typename P, typename T, typename S >
-    struct match_cv
+    struct match_terminal
       : match_placeholder< P, T, S >
     {};
-    template< typename P, typename T, typename S >
-    struct match_cv<
-        P const, T const, S
-    > : match< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_cv<
-        P volatile, T volatile, S
-    > : match< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_cv<
-        P const volatile, T const volatile, S
-    > : match< P, T, S >
-    {};
-
-    // pointers
-    template< typename P, typename T, typename S >
-    struct match_pointer
-      : match_cv< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_pointer<
-        P*, T*, S
-    > : match< P, T, S >
-    {};
-    template< typename P, typename PO, typename T, typename TO, typename S >
-    struct match_pointer<
-        P PO::*, T TO::*, S
-    > : match< linear< P, PO >, linear< T, TO >, S >
-    {};
-
-    // references
-    template< typename P, typename T, typename S >
-    struct match_reference
-      : match_pointer< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_reference<
-        P&, T&, S
-    > : match< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_reference<
-        P&&, T&&, S
-    > : match< P, T, S >
-    {};
-
-    // arrays
-    template< typename P, typename T, typename S >
-    struct match_array
-      : match_reference< P, T, S >
-    {};
-    template< typename P, typename T, typename S >
-    struct match_array< P[], T[], S >
-      : match< P, T, S >
-    {};
-    template< typename P, typename T, int Size, typename S >
-    struct match_array< P[ Size ], T[ Size ], S >
-      : match< P, T, S >
-    {};
-    template< typename P, typename T, int Size, typename S >
-    struct match_array< P[ N ], T[ Size ], S >
-      : match< P, T, S >
-    {};
-
-    // functions
-    template< typename P, typename T, typename S, typename E = void >
-    struct match_function
-      : match_array< P, T, S >
-    {};
-    template< typename PR, typename ...PA, typename TR, typename ...TA, typename S >
-    struct match_function<
-        PR( PA... ), TR( TA... ), S
-    > : match< linear< PR, PA... >, linear< TR, TA... >, S >
-    {};
-    template< typename PR, typename ...PA, typename TR, typename ...TA, typename S >
-    struct match_function<
-        PR( PA..., ... ), TR( TA..., ... ), S
-    > : match< linear< PR, PA... >, linear< TR, TA... >, S >
-    {};
-
-    // templates
-    template< typename P, typename T, typename S, typename E = void >
-    struct match_template
-      : match_function< P, T, S >
-    {};
-    template< template< typename... > class P, typename ...PA, typename T, typename S >
-    struct match_template<
-        P< PA... >, T, S
-      , typename boost::enable_if<
-            is_metafunction< P >
-        >::type
-    > : call< P< PA... >, T, S >
-    {};
-    template< template< typename... > class U, typename ...P, typename ...T, typename S >
-    struct match_template<
-        U< P... >, U< T... >, S
-      , typename boost::disable_if<
-            is_metafunction< U >
-        >::type
-    > : match< linear< P... >, linear< T... >, S >
+    template< typename T, typename S >
+    struct match_terminal<
+        T, T, S
+    > : match_true< S >
     {};
 
     // ...finally
     template< typename P, typename T, typename S >
     struct match
-      : match_template< P, T, S >
+      : match_terminal< P, T, S >
     {};
 
     // linear match
